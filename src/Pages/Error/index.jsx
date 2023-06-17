1a1bfae05bd82b57e2a63748c58b9fc6ffddf871
Julia Taylor <julia.taylor3@gmail.com>
2023-06-17 14:46:42 +0200

    error page created

--- src/Pages/Error/index.jsx
+++ src/Pages/Error/index.jsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom'
+
 function Error() {
     return (
-      <div className="App">
-        Error
-      </div>
-    );
-  }
-  
-  export default Error;
+        <div className="error">
+            <h1 className="error__title">404</h1>
+            <p className= "error__info">Oups ! La page que vous demandez n'existe pas</p>
+            <Link className= "error__link" to="/">Retourner sur la page d'accueil</Link>
+        </div>
+    )
+}
+ 
+export default Error
